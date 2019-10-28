54daac3e8b90eb44fa3443c2af94e275d7b34646
Rokett <rokett@rokett.me>
2019-10-28 16:09:23 +0000

    Simplifying

--- is_dir_empty.go
+++ is_dir_empty.go
@@ -1,22 +1,14 @@
 package main
 
 import (
-	"io"
-	"os"
+	"io/ioutil"
 )
 
 func isDirEmpty(path string) (bool, error) {
-	f, err := os.Open(path)
+	entries, err := ioutil.ReadDir(path)
 	if err != nil {
 		return false, err
 	}
-	defer f.Close()
 
-	// Read a single file and if it is EOF, the directory is empty
-	_, err = f.Readdir(1)
-	if err == io.EOF {
-		return true, nil
-	}
-
-	return false, err
+	return len(entries) == 0, nil
 }
