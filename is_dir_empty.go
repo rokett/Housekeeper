f64cdf7ad0059c4072e03e46622554f7b42a63bc
Rokett <rokett@rokett.me>
2019-10-28 22:49:22 +0000

    Checking subdirectories to see if they have any files in or not

--- is_dir_empty.go
+++ is_dir_empty.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"io/ioutil"
 )
 
@@ -10,5 +11,23 @@ func isDirEmpty(path string) (bool, error) {
 		return false, err
 	}
 
-	return len(entries) == 0, nil
+	// In order to tell whether a directory is empty, we need to check any sub directories as well.
+	for _, dir := range entries {
+		if dir.IsDir() == false {
+			return false, nil
+		}
+
+		subdir := fmt.Sprintf("%s\\%s", path, dir.Name())
+
+		empty, err := isDirEmpty(subdir)
+		if err != nil {
+			return false, err
+		}
+
+		if empty == false {
+			return false, nil
+		}
+	}
+
+	return true, nil
 }
